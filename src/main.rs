d89f030185e768bfd5b63fabaa778ebe1cdab4a4
Sandro Stikić <sandro@stikic.com>
2023-12-08 01:06:24 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use color_eyre::Result;
 use owl::{cec, os};
 use tokio::signal;
-use tracing::{error, info, level_filters::LevelFilter};
+use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
@@ -9,8 +9,8 @@ async fn main() -> Result<()> {
     init_tracing()?;
 
     info!("starting daemon...");
-    let (cec_task, cmd_tx) = cec::spawn_thread();
-    let (os_task, mut event_rx) = os::windows::spawn_thread();
+    let (cec_thread, cmd_tx) = cec::spawn_thread();
+    let (os_thread, mut event_rx) = os::windows::spawn_thread();
 
     while let Some(event) = event_rx.recv().await {
         cmd_tx.send(event.into()).await?;
