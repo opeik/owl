7bf08e7f71421932e9a7c7dd163ed709d5893cc1
Sandro Stikić <sandro@stikic.com>
2024-09-21 01:42:02 +0800

    wip

--- cec_sys/build.rs
+++ cec_sys/build.rs
@@ -8,6 +8,7 @@ use std::{
 
 use cfg_if::cfg_if;
 use color_eyre::eyre::{eyre, Result};
+use target_lexicon::OperatingSystem;
 
 #[derive(Debug)]
 pub enum BuildKind {
@@ -27,29 +28,31 @@ fn main() -> Result<()> {
     color_eyre::install()?;
     let download_path = PathBuf::from(env::var("OUT_DIR")?);
     let lib_path = download_path.join("libcec");
+    let lib_path_str = lib_path.to_string_lossy();
     dbg!(&lib_path);
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        lib_path.to_string_lossy()
-    );
-
+    println!("cargo:rustc-link-search=native={lib_path_str}");
     println!("cargo:rustc-link-lib=static=cec");
     println!("cargo:rustc-link-lib=static=p8-platform");
+    dbg!(target_lexicon::HOST);
 
-    match std::env::consts::OS {
-        "windows" => {}
-        "macos" => {
+    match (target_lexicon::HOST.operating_system, BUILD_KIND) {
+        (OperatingSystem::Windows, BuildKind::Debug) => {
+            println!("cargo:rustc-link-lib=dylib=msvcrtd");
+        }
+        (OperatingSystem::Windows, BuildKind::Release) => {
+            println!("cargo:rustc-link-lib=dylib=msvcrt");
+        }
+        (OperatingSystem::Darwin, _) => {
             println!("cargo:rustc-link-search=framework=/Library/Frameworks");
-
-            println!("cargo:rustc-link-lib=c++");
+            println!("cargo:rustc-link-lib=dylib=c++");
             println!("cargo:rustc-link-lib=framework=CoreVideo");
             println!("cargo:rustc-link-lib=framework=IOKit");
         }
-        "linux" => {
-            println!("cargo:rustc-link-lib=c++");
+        (OperatingSystem::Linux, _) => {
+            println!("cargo:rustc-link-lib=dylib=c++");
         }
-        _ => return Err(eyre!("invalid arch")),
+        _ => return Err(eyre!("unsupported target")),
     };
 
     // Building libcec from source is _painful_.
